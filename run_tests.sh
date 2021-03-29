9fc548e472da2be17703f4e538b73a81d50e1717
Moksh Jawa <mokshjawa@gmail.com>
2021-03-29 11:25:02 -0400

    added ipc flag to prevent chromium rendering error

--- run_tests.sh
+++ run_tests.sh
@@ -1,1 +1,1 @@
-docker build -f test.dockerfile -t tester . && docker run -v /var/run/docker.sock:/var/run/docker.sock -it --network host -v "$(pwd)/recordings:/retool/cypress/videos" tester
+docker build -f test.dockerfile -t tester . && docker run --ipc=host -v /var/run/docker.sock:/var/run/docker.sock -it --network host -v "$(pwd)/recordings:/retool/cypress/videos" tester
